df9b000d77b866ceb8f1393ffbf220738a5b9164
Konstantinos Tsatsarounos <konstantinos.tsatsarounos@gmail.com>
2016-10-14 19:31:46 +0300

    add libs folder and angular

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Konstantinos Tsatsarounos<konstantinos.tsatsarounos@gmail.com>
+ * Project Name:
+ * Filename: ${FILE_NAME}
+ * Date: 14/10/2016
+ * Time: 7:27 μμ
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+    gulp.src([
+	    'node_modules/angular/*.js',
+	    'node_modules/angular/*.css'
+    ]).pipe( gulp.dest('libs/angular') );
+});
